7524bbca085f8c34592ee8f2146bdf7c5ce24432
katestud <kate@automatedinsights.com>
2018-11-08 11:06:54 -0500

    Add HTTPoison for interaction with API

--- temp_tracker_ui/mix.exs
+++ temp_tracker_ui/mix.exs
@@ -39,7 +39,8 @@ defmodule TempTrackerUi.Mixfile do
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
       {:plug_cowboy, "~> 1.0"},
-      {:temp_tracker, path: "../temp_tracker"}
+      {:temp_tracker, path: "../temp_tracker"},
+      {:httpoison, "~> 1.4"}
     ]
   end
 end
