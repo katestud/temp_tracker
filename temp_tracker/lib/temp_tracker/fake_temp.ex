54b79690a51b196e5dbb787f9e991345d463c0be
katestud <kate@automatedinsights.com>
2018-11-08 11:08:03 -0500

    Add function to get latest readings from temp tracker

--- temp_tracker/lib/temp_tracker/fake_temp.ex
+++ temp_tracker/lib/temp_tracker/fake_temp.ex
@@ -11,6 +11,23 @@ defmodule TempTracker.FakeTemp do
     71.25
   end
 
+  def recent_readings(_) do
+    %{
+      measurement_1_temp: 71.45,
+      measurement_1_timestamp: DateTime.utc_now(),
+      measurement_2_temp: 71.45,
+      measurement_2_timestamp: DateTime.utc_now(),
+      measurement_3_temp: 71.45,
+      measurement_3_timestamp: DateTime.utc_now(),
+      measurement_4_temp: 71.45,
+      measurement_4_timestamp: DateTime.utc_now(),
+      measurement_5_temp: 71.45,
+      measurement_5_timestamp: DateTime.utc_now(),
+      measurement_6_temp: 71.45,
+      measurement_6_timestamp: DateTime.utc_now()
+    }
+  end
+
   # GenServer API
 
   def init(_opts) do
