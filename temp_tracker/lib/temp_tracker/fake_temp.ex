e3613807915a283b0b7a1c4b56b274cc83edeb41
Kate Studwell <katestudwell@gmail.com>
2018-11-09 09:00:37 -0500

    Add more temperature readings to fake temp and format time as expected

--- temp_tracker/lib/temp_tracker/fake_temp.ex
+++ temp_tracker/lib/temp_tracker/fake_temp.ex
@@ -11,20 +11,28 @@ defmodule TempTracker.FakeTemp do
     71.25
   end
 
-  def recent_readings(_) do
+  def recent_readings do
     %{
-      measurement_1_temp: 71.45,
-      measurement_1_timestamp: DateTime.utc_now(),
+      measurement_1_temp: 71.20,
+      measurement_1_timestamp: current_time(),
       measurement_2_temp: 71.45,
-      measurement_2_timestamp: DateTime.utc_now(),
+      measurement_2_timestamp: current_time(),
       measurement_3_temp: 71.45,
-      measurement_3_timestamp: DateTime.utc_now(),
+      measurement_3_timestamp: current_time(),
       measurement_4_temp: 71.45,
-      measurement_4_timestamp: DateTime.utc_now(),
+      measurement_4_timestamp: current_time(),
       measurement_5_temp: 71.45,
-      measurement_5_timestamp: DateTime.utc_now(),
+      measurement_5_timestamp: current_time(),
       measurement_6_temp: 71.45,
-      measurement_6_timestamp: DateTime.utc_now()
+      measurement_6_timestamp: current_time(),
+      measurement_7_temp: 71.45,
+      measurement_7_timestamp: current_time(),
+      measurement_8_temp: 71.45,
+      measurement_8_timestamp: current_time(),
+      measurement_9_temp: 71.45,
+      measurement_9_timestamp: current_time(),
+      measurement_10_temp: 71.23,
+      measurement_10_timestamp: current_time()
     }
   end
 
@@ -37,6 +45,13 @@ defmodule TempTracker.FakeTemp do
     }
 
     {:ok, state}
+  end
+
+  defp current_time do
+    DateTime.utc_now
+    |> DateTime.truncate(:second)
+    |> DateTime.to_string
+    |> String.slice(0..18)
   end
 
 end
