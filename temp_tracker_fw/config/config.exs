55879fdfcdfa5c1a32f9f23fea9bffbfe70832eb
Kate Studwell <katestudwell@gmail.com>
2018-11-28 20:58:07 -0500

    Revert back to using wlan by default

--- temp_tracker_fw/config/config.exs
+++ temp_tracker_fw/config/config.exs
@@ -45,10 +45,10 @@ config :nerves_firmware_ssh,
 # See https://hexdocs.pm/nerves_init_gadget/readme.html for more information.
 
 config :nerves_init_gadget,
-  ifname: "usb0",
-  address_method: :dhcpd,
+  ifname: "wlan0",
+  address_method: :dhcp,
   mdns_domain: "nerves.local",
-  node_name: nil,
+  node_name: :temp_tracker_fw,
   node_host: :mdns_domain,
   ssh_console_port: 22
 
