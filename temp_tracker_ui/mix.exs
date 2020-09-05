0495a33dc5ccc9a72d931b4fd55f577ba847460d
Kate Studwell <katestudwell@gmail.com>
2020-09-05 12:23:36 -0400

    Upgrade to phoenix 1.4

--- temp_tracker_ui/mix.exs
+++ temp_tracker_ui/mix.exs
@@ -32,15 +32,16 @@ defmodule TempTrackerUi.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.4"},
+      {:phoenix, "~> 1.4.0"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
       {:plug_cowboy, "~> 1.0"},
+      {:plug, "~> 1.7"},
       {:temp_tracker, path: "../temp_tracker"},
-      {:httpoison, "~> 1.4"}
+      {:httpoison, "~> 1.4"},
+      {:poison, "~> 3.1"}
     ]
   end
 end
